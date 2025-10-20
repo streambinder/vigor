c7da1086521754dc7ca7bee9d885b84093d29eb1
streambinder <posta@davidepucci.it>
2025-10-20 21:46:28 +0200

    refactor: lint codebase

--- server/flow.sh
+++ server/flow.sh
@@ -9,11 +9,11 @@ echo "$TOKENS" | jq
 ACCESS=$(echo "$TOKENS" | jq -r .access_token)
 REFRESH=$(echo "$TOKENS" | jq -r .refresh_token)
 
-curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS"  -X POST http://localhost:8080/user/update \
-    -d '{"birthdate":"01/01/2000","language":"it","height":180,"weight":60,"data":{"goals":["hypertrophy","hyperlordosis compensation"]}}' | jq
+curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS" -X POST http://localhost:8080/user/update \
+	-d '{"birthdate":"01/01/2000","language":"it","height":180,"weight":60,"data":{"goals":["hypertrophy","hyperlordosis compensation"]}}' | jq
 curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $ACCESS" http://localhost:8080/user | jq
 
-NEW=$(curl -s -H "Content-Type: application/json" -X POST http://localhost:8080/refresh -d '{"refresh_token":"'$REFRESH'"}')
+NEW=$(curl -s -H "Content-Type: application/json" -X POST http://localhost:8080/refresh -d '{"refresh_token":"'"$REFRESH"'"}')
 echo "$NEW" | jq
 ACCESS=$(echo "$NEW" | jq -r .access_token)
 REFRESH=$(echo "$NEW" | jq -r .refresh_token)
