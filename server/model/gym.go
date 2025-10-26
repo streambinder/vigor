95018c7ce52c2932e684c5162ed865f8553f8cd9
streambinder <posta@davidepucci.it>
2025-10-26 11:27:23 +0100

    fix(model): align Gym struct fields for gofumpt compliance

--- server/model/gym.go
+++ server/model/gym.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Gym struct {
-	ID          uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
-	Name        string    `gorm:"not null;uniqueIndex:idx_user_gym_name" json:"name"`
-	Equipment   pq.StringArray  `gorm:"type:text[]" json:"equipment"`
+	ID        uuid.UUID      `gorm:"type:uuid;default:uuid_generate_v4();primaryKey" json:"id"`
+	Name      string         `gorm:"not null;uniqueIndex:idx_user_gym_name" json:"name"`
+	Equipment pq.StringArray `gorm:"type:text[]" json:"equipment"`
 
 	CreatedAt time.Time      `json:"-"`
 	UpdatedAt time.Time      `json:"-"`
