e3a88ad05d8c9690ca1f304b878ecfe0ffe232cb
streambinder <posta@davidepucci.it>
2025-10-29 16:36:49 +0100

    feat(server): add CORS middleware for web/mobile clients
    Enable cross-origin requests by configuring CORS middleware to allow
    all origins, standard HTTP methods, and necessary headers for
    authentication.

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -2,7 +2,18 @@ package handler
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 // APP is the global Fiber application instance for all HTTP handlers.
 var APP = fiber.New()
+
+func init() {
+	// Configure CORS to allow requests from web/mobile clients
+	APP.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
+		AllowCredentials: false,
+	}))
+}
