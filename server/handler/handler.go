1b4e17593b2315f805f000ef70ac34c835e1e094
streambinder <posta@davidepucci.it>
2025-11-01 17:03:32 +0100

    feat(server): add HTTP request logging middleware
    Add zerolog middleware to log all HTTP requests with method, path,
    status code, processing duration, and client IP address.

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/rs/zerolog/log"
 )
 
 // APP is the global Fiber application instance for all HTTP handlers.
@@ -16,4 +19,26 @@ func init() {
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization",
 		AllowCredentials: false,
 	}))
+
+	// Add logging middleware to track request processing time
+	APP.Use(func(c *fiber.Ctx) error {
+		start := time.Now()
+
+		// Process request
+		err := c.Next()
+
+		// Calculate duration
+		duration := time.Since(start)
+
+		// Log request details
+		log.Info().
+			Str("method", c.Method()).
+			Str("path", c.Path()).
+			Int("status", c.Response().StatusCode()).
+			Dur("duration", duration).
+			Str("ip", c.IP()).
+			Msg("HTTP request")
+
+		return err
+	})
 }
