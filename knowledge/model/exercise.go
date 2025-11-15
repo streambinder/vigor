ff12ac77034c50163550161e0a20d24ca648c71b
streambinder <posta@davidepucci.it>
2025-11-15 14:26:55 +0100

    refactor(knowledge): merge exercise related models

--- knowledge/model/exercise.go
+++ knowledge/model/exercise.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/lib/pq"
+	"github.com/pgvector/pgvector-go"
 	"gorm.io/gorm"
 )
 
@@ -20,4 +21,17 @@ type Exercise struct {
 	CreatedAt time.Time      `json:"-"`
 	UpdatedAt time.Time      `json:"-"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+}
+
+type ExerciseEmbedding struct {
+	ID        uint            `gorm:"primaryKey" json:"id"`
+	Text      string          `gorm:"type:text;not null" json:"text"` // The text that was embedded
+	Embedding pgvector.Vector `gorm:"type:vector(384)" json:"-"`      // all-MiniLM-L6-v2 embedding dimension is 384
+
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	ExerciseID string   `gorm:"type:varchar(255);not null;uniqueIndex:idx_exercise_embedding" json:"exercise_id"`
+	Exercise   Exercise `gorm:"foreignKey:ExerciseID;references:ID" json:"exercise"`
 }
